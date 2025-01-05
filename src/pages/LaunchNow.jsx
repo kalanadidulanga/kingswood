96324eb5db9d619149abf140e3e2f8cb9dba5a59
Kalana Didulanga Koralegedara <dev.kalanadidulanga@gmail.com>
2025-01-05 21:56:11 +0530

    fixed

--- src/pages/LaunchNow.jsx
+++ src/pages/LaunchNow.jsx
@@ -1,4 +1,4 @@
-import { Loader2 } from "lucide-react";
+import { ArrowRight, Loader2 } from "lucide-react";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -10,21 +10,44 @@ const LaunchNow = () => {
     setTimeout(() => {
       navigate("/");
       setIsLoading(false);
-    }, 3000);
+    }, 5000);
   };
   return (
-    <div className=" bg-slate-500 w-dvw h-dvh flex flex-col flex-1 justify-center items-center">
-      <img src="/gifs/1.gif" alt="roket" />
-      <button
-        className="px-4 py-2 rounded-lg transition-colors bg-red-500 text-white flex items-center gap-2"
-        onClick={() => {
-          handleLaunch();
-        }}
-      >
-        {isLoading && <Loader2 className="animate-spin" />}
-        {/* <Loader2 className="animate-spin" /> */}
-        {isLoading ? "Launching..." : "Launch Now"}
-      </button>
+    <div className=" bg-slate-50 w-dvw h-dvh flex flex-col flex-1 justify-center items-center">
+      {/* <img src="/gifs/1.gif" alt="roket" /> */}
+      <img
+        src="/gifs/2.gif"
+        alt="count"
+        className={`absolute transform w-full h-full z-50 ${
+          isLoading ? "block" : "hidden"
+        }`}
+      />
+
+      <img
+        src="/img/bg.png"
+        alt="count"
+        className={`absolute transform w-full h-full`}
+      />
+
+      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+        <div className="">
+          <img src="/logo.png" alt="logo" />
+        </div>
+
+        {!isLoading && (
+          <button
+            className="px-5 py-2 rounded-lg transition-all bg-primary text-white flex items-center gap-2 text-xl font-semibold hover:bg-primary/80 mt-8 tr duration-300 hover:scale-105 hover:shadow-lg hover:gap-8"
+            onClick={() => {
+              handleLaunch();
+            }}
+          >
+            {/* {isLoading && <Loader2 className="animate-spin" />} */}
+            {/* <Loader2 className="animate-spin" /> */}
+            {isLoading ? "Launching..." : "Launch Now"}
+            <ArrowRight className=" " />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#af0c00",
+        secondary: "#00FF00",
+        background: "#000000",
+      },
+    },
   },
   plugins: [],
 }
